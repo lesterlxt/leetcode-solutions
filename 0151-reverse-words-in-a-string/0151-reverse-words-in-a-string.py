4daf94b6869c13f1881023c20832b1fdd44ca4ee
Lester Liang <153183032+lesterlxt@users.noreply.github.com>
2026-01-19 23:42:05 +1100

    Time: 4 ms (17.96%), Space: 19.4 MB (10.45%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -5,7 +5,7 @@ class Solution:
 
         while r >= 0:
             if s[r] != " ":
-                l = r - 1
+                l = r
                 while l >= 0 and s[l] != " ":
                     l -= 1
                 words.append(s[l + 1: r + 1])
