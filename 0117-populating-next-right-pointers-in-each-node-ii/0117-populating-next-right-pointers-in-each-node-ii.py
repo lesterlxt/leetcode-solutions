f806b1f8875aa7487f15fa4fd06097c3c1aa2a4e
Lester Liang <153183032+lesterlxt@users.noreply.github.com>
2026-02-16 21:07:52 +0800

    Time: 44 ms (82.77%), Space: 20.6 MB (5.21%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -10,28 +10,20 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        if not root:
-            return None
+        cur = root
+        while cur:
+            dummy = Node(0)
+            tail = dummy
+            while cur:
+                if cur.left:
+                    tail.next = cur.left
+                    tail = tail.next
+                if cur.right:
+                    tail.next = cur.right
+                    tail = tail.next
+                cur = cur.next
+            
+            cur = dummy.next
         
-        q = deque()
-        q.append(root)
-
-        while q:
-            level_size = len(q)
-            prev = None
-
-            for _ in range(level_size):
-                node = q.popleft()
-                if prev:
-                    prev.next = node
-
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-                
-                prev = node
-
-            prev.next = None
-
         return root
+            
