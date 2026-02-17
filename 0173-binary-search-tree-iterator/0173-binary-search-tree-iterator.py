237f09038215c430b320fb06a6dd1604218e56a3
Lester Liang <153183032+lesterlxt@users.noreply.github.com>
2026-02-18 04:55:11 +0800

    Time: 7 ms (60.95%), Space: 25.7 MB (39.91%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -7,23 +7,23 @@
 class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
-        self.stack: list[TreeNode] = []
-        self._push_left_branch(root)
+        self.stack = []
+        self._push_left(root)
 
-    def _push_left_branch(self, node: Optional[TreeNode]) -> None: 
-        while node is not None:
+    def _push_left(self, node):
+        while node:
             self.stack.append(node)
             node = node.left
 
     def next(self) -> int:
         node = self.stack.pop()
-        val = node.val
-        if node.right is not None:
-            self._push_left_branch(node.right)
-        return val
+        if node.right:
+            self._push_left(node.right)
+        return node.val
 
     def hasNext(self) -> bool:
         return len(self.stack) > 0
+        
 
 
 # Your BSTIterator object will be instantiated and called as such:
