a73623f3b3d749b0ecbe5306da1759fc6e6d494d
Lester Liang <153183032+lesterlxt@users.noreply.github.com>
2026-03-01 02:31:44 +0800

    Time: 7 ms (29.19%), Space: 20.4 MB (81.06%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,29 +1,26 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = [[] for _ in range(numCourses)]
-        indegree = [0] * numCourses
+        graph = defaultdict(list)
+        indeg = [0] * numCourses
 
-        for a, b in prerequisites:
-            graph[b].append(a)
-            indegree[a] += 1
-        
-        q = collections.deque()
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                q.append(i)
-        
         order = []
+        q = deque()
+        for a, b in prerequisites:
+            graph[b].append(a)  # b -> a
+            indeg[a] += 1
+        
+        for course in range(numCourses):
+            if indeg[course] == 0:
+                q.append(course)
+        
         while q:
-            course = q.popleft()
-            order.append(course)
+            cur = q.popleft()
+            order.append(cur)
 
-            for nxt in graph[course]:
-                indegree[nxt] -= 1
-                if indegree[nxt] == 0:
+            for nxt in graph[cur]:
+                indeg[nxt] -= 1
+                if indeg[nxt] == 0:
                     q.append(nxt)
         
-        if len(order) == numCourses:
-            return order
-        else:
-            return []
+        return order if len(order) == numCourses else []
 
